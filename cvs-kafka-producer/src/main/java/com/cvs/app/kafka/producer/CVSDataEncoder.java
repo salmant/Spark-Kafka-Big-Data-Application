d4eed4b90fc783d2fa58bb9fd3a29085d580601a
salmant <salman.taherizadeh@gmail.com>
2019-12-16 18:15:01 +0100

    Update CVSDataEncoder.java

--- cvs-kafka-producer/src/main/java/com/cvs/app/kafka/producer/CVSDataEncoder.java
+++ cvs-kafka-producer/src/main/java/com/cvs/app/kafka/producer/CVSDataEncoder.java
@@ -11,18 +11,24 @@ import kafka.utils.VerifiableProperties;
 // This code is published under the Apache 2 license
 // ------------------------------------------------------------------------
 
+/*
+ * Class to serialise the CVS data messages.
+ */
+
 public class CVSDataEncoder implements Encoder<CVSData> {
 	
 	private static final Logger logger = Logger.getLogger(CVSDataEncoder.class);	
 	private static ObjectMapper objectMapper = new ObjectMapper();		
 	
+	//supplying a "VerifiableProperties" constructor is required
 	public CVSDataEncoder(VerifiableProperties verifiableProperties) {
 
-    }
+	}
 	
 	/*
 	 * Method to serialise CVSData objects.
 	 */
+	//"toBytes(...)" method to return a byte array
 	public byte[] toBytes(CVSData cvsEvent_toBytes) {
 		try {
 			String msg = objectMapper.writeValueAsString(cvsEvent_toBytes);
